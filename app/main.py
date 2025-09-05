265de4d697cdb034bc42b03f058a1cb50becc670
mark <mark.lavov@gmail.com>
2025-09-05 05:29:42 +0300

    localhost:8000

--- app/main.py
+++ app/main.py
@@ -5,3 +5,7 @@ app = FastAPI()
 
 app.include_router(questions)
 app.include_router(answers)
+
+@app.get("/")
+def root():
+    return {"message": "API работает. Перейдите на /docs для документации"}
